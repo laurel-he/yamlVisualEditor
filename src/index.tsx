1c078d52aa974bc7c43a205ea32e88d713a16085
贺小娇 <>
2021-01-13 17:36:34 +0800

    add webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,4 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import "./index.css";
-import App from './views/app.tsx';
-// import { Resizer } from './views/Resizer';
-const targetRef = document.getElementById('root')
-
-function initApp() {
-  ReactDOM.render(<App />, targetRef, () => {
-    window['reactDomRenderReady'] && window['reactDomRenderReady']()
-    // window.addEventListener('resize', new Resizer(targetRef).resizeHandler)
-  })
-}
-initApp();
-
-// if (module['hot']) {
-//   module['hot'].accept(['./views/app'], () => {
-//     initApp();
-//   })
-// }
+ReactDOM.render((<div>hello  webpack !!!</div>), document.getElementById("root"))
